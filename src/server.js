117628055a80dce113fd09f223cfb047498eb619
Camilo <camiloa.mejia@udea.edu.co>
2021-04-01 13:02:18 -0500

    Testing github webhook

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const { configs } = require('./config/index');
 
 let port = configs.port;
 let host = configs.host;
-
+console.log('Testing webhook');
 app.listen(port, host, () => {
   console.log(`Server on port http://${host}:${port}`);
 });
