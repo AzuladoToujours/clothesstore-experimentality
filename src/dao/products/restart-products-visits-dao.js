784c483d035ad84ed466d7878a76c94a91fb3770
Camilo <camiloa.mejia@udea.edu.co>
2021-04-02 15:14:00 -0500

    Improving visits reset DAO

--- src/dao/products/restart-products-visits-dao.js
+++ src/dao/products/restart-products-visits-dao.js
@@ -1,12 +1,18 @@
 const models = require('../../database/models');
-
+const sequelize = require('sequelize');
 /**
  * Restarts visits in Products
  */
 
 const restartProductsVisitsDAO = async () => {
   try {
-    const products = await models.Products.findAll();
+    const products = await models.Products.findAll({
+      where: {
+        visits: {
+          [sequelize.Op.gt]: 0,
+        },
+      },
+    });
 
     products.map(async (product) => {
       product.visits = 0;
